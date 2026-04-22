cf6be9bdf32d3ecce2be172aed6c33fb35c9def8
Nivetha-1235 <nivethavenkatraman48@gmail.com>
2026-04-22 22:33:34 +0530

    github config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Nivii_portfolio',
+  assetPrefix: '/Nivii_portfolio',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
